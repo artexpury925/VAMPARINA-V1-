0b67effd75bccd2a8278d6308aee6ce0b02cd4f5
artexpury925 <artexpury@gmail.com>
2025-11-18 21:12:11 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,11 @@
+// settings.mjs — VAMPARINA V1 EMPIRE SETTINGS (ES MODULE 2025)
 const settings = {
-  packname: 'VAMPARINA V1',
-  author: 'ARNOLD CHIRCHIR‎',
-  botName: "VAMPARINA V1",
-  botOwner: 'ARNOLD CHIRCHIR', // Your name
-  ownerNumber: '254703110780', //Set your number here without + symbol, just add country code & number without any space
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  maxStoreMessages: 20, 
-  storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.2.0",
-  updateZipUrl: "https://github.com/arnold6001/VAMPARINA-V1/archive/refs/heads/main.zip",
-};
+    ownerNumber: "254703110780",
+    botName: "VAMPARINA V1",
+    prefix: ".",
+    empireName: "ETERNAL EMPIRE",
+    sudoList: ["254703110780@s.whatsapp.net"],
+    globalPrefix: true
+}
 
-module.exports = settings;
+export default settings
