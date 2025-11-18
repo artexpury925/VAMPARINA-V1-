308476a3902a1e3fe60baa7b5ed342a61b1c60e8
artexpury925 <artexpury@gmail.com>
2025-11-18 22:11:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
     ownerNumber: "254703110780",
     botName: "VAMPARINA V1",
     prefix: ".",
-    EMPIRE_GROUP_INVITE: "BZNDaKhvMFo5Gmne3wxt9n", // ← CHANGE THIS
+    EMPIRE_GROUP_INVITE: "BZNDaKhvMFo5Gmne3wxt9n", // Change to your group invite code
     EMPIRE_CHANNEL: "0029VbBm7apIXnlmuyjGGM0p"
-}
+};
