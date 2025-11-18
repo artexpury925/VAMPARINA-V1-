83783f46e652027d8102e464cfa1fe06eeb1fba3
artexpury925 <artexpury@gmail.com>
2025-11-18 21:13:08 +0300

    Create settings.mjs

---
+++ settings.mjs
@@ -0,0 +1,11 @@
+// settings.mjs — VAMPARINA V1 EMPIRE SETTINGS (ES MODULE 2025)
+const settings = {
+    ownerNumber: "254703110780",
+    botName: "VAMPARINA V1",
+    prefix: ".",
+    empireName: "ETERNAL EMPIRE",
+    sudoList: ["254703110780@s.whatsapp.net"],
+    globalPrefix: true
+}
+
+export default settings
