ea1443cdcfd48b3d4d078cbda190216b6799b50b
artexpury925 <artexpury@gmail.com>
2025-11-18 21:29:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
-// config.js — VAMPARINA V1 SETTINGS (COMMONJS)
+// config.js — VAMPARINA V1 SETTINGS
 module.exports = {
     ownerNumber: "254703110780",
     botName: "VAMPARINA V1",
     prefix: ".",
-    empireName: "ETERNAL EMPIRE",
-    sudoList: ["254703110780@s.whatsapp.net"],
-    globalPrefix: true,
-    EMPIRE_GROUP_INVITE: "BZNDaKhvMFo5Gmne3wxt9n", // ← CHANGE TO YOUR GROUP
+    EMPIRE_GROUP_INVITE: "BZNDaKhvMFo5Gmne3wxt9n", // ← CHANGE THIS
     EMPIRE_CHANNEL: "0029VbBm7apIXnlmuyjGGM0p"
 }
